7adeb1099de565f6f3b42c676a8bf151a8e2e5a1
Unknown <petinari@outlook.com>
2018-05-04 14:18:36 -0400

    2

--- vega2/vega2/Controllers/ValuesController.cs
+++ vega2/vega2/Controllers/ValuesController.cs
@@ -13,7 +13,7 @@ namespace vega2.Controllers
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "value1", "val" };
         }
 
         // GET api/values/5
